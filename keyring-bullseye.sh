724db304bd5bf8a4c197ca50ad9688046809eb8b
rutantan <cesarpk74@gmail.com>
2025-03-28 15:59:03 +0000

    Add files via upload

---
+++ keyring-bullseye.sh
@@ -0,0 +1,9 @@
+# Install signing keyring
+keyring="$(mktemp)"
+version="$(curl -L https://github.com/radxa-pkg/radxa-archive-keyring/releases/latest/download/VERSION)"
+curl -L --output "$keyring" "https://github.com/radxa-pkg/radxa-archive-keyring/releases/latest/download/radxa-archive-keyring_${version}_all.deb"
+sudo dpkg -i "$keyring"
+rm -f "$keyring"
+# Add apt package repo
+sudo tee /etc/apt/sources.list.d/70-radxa.list <<< "deb [signed-by=/usr/share/keyrings/radxa-archive-keyring.gpg] https://radxa-repo.github.io/bullseye/ bullseye main"
+sudo apt-get update
